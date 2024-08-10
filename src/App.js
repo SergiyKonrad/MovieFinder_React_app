58c5521eb2b4eec992bc2c6312c59a6bf5c6e136
Sergiy Kondratenko <43180380+SergiyKonrad@users.noreply.github.com>
2024-08-09 23:28:39 -0400

    const API_URL = 'http changed to = https in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard'
 import './App.css'
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7'
+const API_URL = 'https://www.omdbapi.com?apikey=c032e2d7'
 
 // const movie1 = {
 //   Title: 'Amazing Spiderman Syndrome',
